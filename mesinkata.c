499d470c64b70201c999bb3f318ddadabcd224c7
audrynyonata <audrynyonata@gmail.com>
2016-11-05 15:33:06 +0700

    Add files via upload

---
+++ mesinkata.c
@@ -0,0 +1,131 @@
+/* File: mesinkata.c */
+/* Implementasi Mesin Kata: Model Akuisisi Versi I */
+
+#include "mesinkata.h"
+#include <stdio.h>
+
+boolean EndKata;
+Kata CKata;
+
+void IgnoreBlank()
+/* Mengabaikan satu atau beberapa BLANK
+   I.S. : CC sembarang 
+   F.S. : CC ≠ BLANK atau CC = MARK */
+{
+	while ((CC == BLANK) && (CC != MARK))
+	{	
+		ADV();
+	}
+}
+
+void STARTKATA()
+/* I.S. : CC sembarang 
+   F.S. : EndKata = true, dan CC = MARK; 
+          atau EndKata = false, CKata adalah kata yang sudah diakuisisi,
+          CC karakter pertama sesudah karakter terakhir kata */
+{
+	START();
+	IgnoreBlank();
+	if (CC == MARK)
+	{
+		EndKata = true;
+	}
+	else
+	{
+		EndKata = false;
+		SalinKata();
+		IgnoreBlank();
+	}
+}
+
+void ADVKATA()
+/* I.S. : CC adalah karakter pertama kata yang akan diakuisisi 
+   F.S. : CKata adalah kata terakhir yang sudah diakuisisi, 
+          CC adalah karakter pertama dari kata berikutnya, mungkin MARK
+          Jika CC = MARK, EndKata = true.		  
+   Proses : Akuisisi kata menggunakan procedure SalinKata */
+{
+	IgnoreBlank();
+	if (CC == MARK)
+	{
+		EndKata = true;
+	}
+	else
+	{
+		SalinKata();
+	}
+	IgnoreBlank();
+}
+
+void SalinKata()
+/* Mengakuisisi kata, menyimpan dalam CKata
+   I.S. : CC adalah karakter pertama dari kata
+   F.S. : CKata berisi kata yang sudah diakuisisi; 
+          CC = BLANK atau CC = MARK; 
+          CC adalah karakter sesudah karakter terakhir yang diakuisisi.
+          Jika panjang kata melebihi NMax, maka sisa kata "dipotong" */
+{
+	int i = 1;
+	
+	for (;;)
+	{
+		CKata.TabKata[i] = CC;
+		ADV();
+		if ((CC == MARK ) || (CC == BLANK ))
+		{
+			break;
+		}
+		else
+		{
+			i++;
+		}
+	}
+	if (i>NMax)
+	{
+		CKata.Length=NMax;
+	}
+	else
+	{
+		CKata.Length = i;
+	}	
+}
+
+void InputUser(char *c)
+{
+	scanf("%s", c);
+}
+
+void PrintKata (Kata K)
+/* mencetak Kata K tanpa karakter apapun diawal dan diakhir */
+{
+	int i =1;
+	while (i<=K.Length)
+	{
+		printf("%c", K.TabKata[i]);
+		i++;
+	}
+}
+
+int KataToInt (Kata K)
+{
+	int val = 0;
+	int i = 1;
+	while (i<=K.Length)
+	{
+		val = val * 10;
+		switch (K.TabKata[i]){
+			case '0' : val += 0; break;
+			case '1' : val += 1; break;
+			case '2' : val += 2; break;
+			case '3' : val += 3; break;
+			case '4' : val += 4; break;
+			case '5' : val += 5; break;
+			case '6' : val += 6; break;
+			case '7' : val += 7; break;
+			case '8' : val += 8; break;
+			case '9' : val += 9; break;
+		};
+		i++;
+	}
+	return val;	
+}
