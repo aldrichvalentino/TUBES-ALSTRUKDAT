ac54ca708da10c44b070a36ca3305a598c36bbab
girvandip <girvandip@gmail.com>
2016-11-27 17:59:03 +0700

    Add files via upload

--- skilltree.h
+++ skilltree.h
@@ -2,16 +2,16 @@
 #define _SKILTREE_H_
 
 /* Modul lain yang digunakan : */
-#include "listrek.h"
 #include "boolean.h"
 #include <stdlib.h>
 #include <stdio.h>
 #include "color.h"
 
-/* #define Nil NULL */ /* konstanta Nil sesuai pada modul listrek */
+
+#define Nil NULL /* konstanta Nil sesuai pada modul listrek */
 
 /* *** Definisi Type Pohon Biner *** */
-/* typedef int infotype; */ /* type infotype sesuai pada modul listrek */
+typedef int infotype; /* type infotype sesuai pada modul listrek */
 typedef struct tNode *addrNode;
 typedef struct tNode {
 	infotype info;
@@ -56,101 +56,13 @@ boolean IsTreeEmpty (BinTree P);
 /* Mengirimkan true jika P adalah pohon biner kosong */
 boolean IsTreeOneElmt (BinTree P);
 /* Mengirimkan true jika P adalah pohon biner tidak kosong dan hanya memiliki 1 elemen */
-boolean IsUnerLeft (BinTree P);
-/* Mengirimkan true jika pohon biner tidak kosong P adalah pohon unerleft: hanya mempunyai subpohon kiri */
-boolean IsUnerRight (BinTree P);
-/* Mengirimkan true jika pohon biner tidak kosong P adalah pohon unerright: hanya mempunyai subpohon kanan*/
-boolean IsBiner (BinTree P);
-/* Mengirimkan true jika pohon biner tidak kosong P adalah pohon biner: mempunyai subpohon kiri dan subpohon kanan*/
 
-/* *** Traversal *** */
-void PrintPreorder (BinTree P);
-/* I.S. P terdefinisi */
-/* F.S. Semua simpul P sudah dicetak secara preorder: akar, pohon kiri, dan pohon kanan.
-   Setiap pohon ditandai dengan tanda kurung buka dan kurung tutup ().
-   Pohon kosong ditandai dengan ().
-   Tidak ada tambahan karakter apa pun di depan, tengah, atau akhir. */
-/* Contoh:
-   (A()()) adalah pohon dengan 1 elemen dengan akar A
-   (A(B()())(C()())) adalah pohon dengan akar A dan subpohon kiri (B()()) dan subpohon kanan (C()()) */
-void PrintInorder (BinTree P);
-/* I.S. P terdefinisi */
-/* F.S. Semua simpul P sudah dicetak secara inorder: pohon kiri, akar, dan pohon kanan.
-   Setiap pohon ditandai dengan tanda kurung buka dan kurung tutup ().
-   Pohon kosong ditandai dengan ().
-   Tidak ada tambahan karakter apa pun di depan, tengah, atau akhir. */
-/* Contoh:
-   (()A()) adalah pohon dengan 1 elemen dengan akar A
-   ((()B())A(()C())) adalah pohon dengan akar A dan subpohon kiri (()B()) dan subpohon kanan (()C()) */
-void PrintPostorder (BinTree P);
-/* I.S. P terdefinisi */
-/* F.S. Semua simpul P sudah dicetak secara postorder: pohon kiri, pohon kanan, dan akar.
-   Setiap pohon ditandai dengan tanda kurung buka dan kurung tutup ().
-   Pohon kosong ditandai dengan ().
-   Tidak ada tambahan karakter apa pun di depan, tengah, atau akhir. */
-/* Contoh:
-   (()()A) adalah pohon dengan 1 elemen dengan akar A
-   ((()()B)(()()C)A) adalah pohon dengan akar A dan subpohon kiri (()()B) dan subpohon kanan (()()C) */
 void PrintTree1(BinTree P, int h, int inc);
-void PrintTree (BinTree P, int h);
-/* I.S. P terdefinisi, h adalah jarak indentasi (spasi) */
-/* F.S. Semua simpul P sudah ditulis dengan indentasi (spasi) */
-/* Penulisan akar selalu pada baris baru (diakhiri newline) */
-/* Contoh, jika h = 2:
-1) Pohon preorder: (A()()) akan ditulis sbb:
-A
-2) Pohon preorder: (A(B()())(C()())) akan ditulis sbb:
-A
-  B
-  C
-3) Pohon preorder: (A(B(D()())())(C()(E()()))) akan ditulis sbb:
-A
-  B
-    D
-  C
-    E
-*/
+// Prosedur antara untuk PrintSkillTree
 
 /* *** Searching *** */
 boolean SearchTree (BinTree P, infotype X);
 /* Mengirimkan true jika ada node dari P yang bernilai X */
-
-/* *** Fungsi-Fungsi Lain *** */
-int NbElmt (BinTree P);
-/* Mengirimkan banyaknya elemen (node) pohon biner P */
-int NbDaun (BinTree P);
-/* Mengirimkan banyaknya daun (node) pohon biner P */
-/* Prekondisi: P tidak kosong */
-boolean IsSkewLeft (BinTree P);
-/* Mengirimkan true jika P adalah pohon condong kiri */
-/* Pohon kosong adalah pohon condong kiri */
-boolean IsSkewRight (BinTree P);
-/* Mengirimkan true jika P adalah pohon condong kanan */
-/* Pohon kosong adalah pohon condong kanan */
-int Level (BinTree P, infotype X);
-/* Mengirimkan level dari node X yang merupakan salah satu simpul dari pohon biner P.
-   Akar(P) level-nya adalah 1. Pohon P tidak kosong dan elemen-elemennya unik. */
-int Tinggi (BinTree P);
-/* Pohon Biner mungkin kosong. Tinggi pohon kosong = 0.
-   Mengirim "height" yaitu tinggi dari pohon */
-
-/* *** Operasi lain *** */
-void AddDaunTerkiri (BinTree *P, infotype X);
-/* I.S. P boleh kosong */
-/* F.S. P bertambah simpulnya, dengan X sebagai simpul daun terkiri */
-void AddDaunTerkanan (BinTree *P, infotype X);
-void AddDaun (BinTree *P, infotype X, infotype Y, boolean Kiri);
-/* I.S. P tidak kosong, X adalah salah satu daun Pohon Biner P */
-/* F.S. P bertambah simpulnya, dengan Y sebagai anak kiri X (jika Kiri = true), atau
-        sebagai anak Kanan X (jika Kiri = false) */
-/*		Jika ada > 1 daun bernilai X, diambil daun yang paling kiri */
-void DelDaunTerkiri (BinTree *P, infotype *X);
-/* I.S. P tidak kosong */
-/* F.S. P dihapus daun terkirinya, dan didealokasi, dengan X adalah info yang semula
-        disimpan pada daun tevoid CreateSkillTree(BinTree *P);rkiri yang dihapus */
-void DelDaun (BinTree *P, infotype X);
-/* I.S. P tidak kosong, minimum ada 1 daun bernilai X. */
-/* F.S. Semua daun bernilai X dihapus dari P. */
 
 BinTree SearchBST (BinTree P, infotype X);
 // Mendapatkan pohon yang memiliki akar bernilai X
@@ -159,6 +71,7 @@ boolean IsSudah(BinTree P, infotype X);
 // Mengecek apakah skill sudah diambil atau belum
 
 void SaveSkillTree(BinTree P, FILE *F);
+/* Untuk menyimpan Tree ke file eksternal */
 
 void CreateNewSkillTree(BinTree *P);
 // Membuat skill tree baru untuk player
@@ -167,6 +80,6 @@ int SkillToInt(int skill);
 // Mengubah biar jadi BST kalo di proses di fungsi
 
 void AmbilSkill(BinTree *P);
-// Prosedur untuk ngambil skill
+// Prosedur untuk mengambil skill
 
 #endif
