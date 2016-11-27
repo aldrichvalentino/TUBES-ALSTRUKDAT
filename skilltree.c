3396f975e3f9e09b6d7ba0c6654e8bc24ef6427e
aldrichvalentino <aldrich.vh97@gmail.com>
2016-11-27 22:28:39 +0700

    Add files via upload

--- skilltree.c
+++ skilltree.c
@@ -112,41 +112,41 @@ void PrintTree1(BinTree P, int h, int inc)
         if (Sudah(P))
         {
             switch (Akar(P)){
-                case 8 : printf(COLOR_GREEN     "(1) PIRATES HAT "     COLOR_RESET "\n");break;
-                case 4 : printf(COLOR_GREEN     "(2) CAPTAINS BLOOD"     COLOR_RESET "\n");break;
-                case 12 : printf(COLOR_GREEN    "(3) CAPTAINS SENSE"     COLOR_RESET "\n");break;
-                case 2 : printf(COLOR_GREEN     "(4) BLOODY HOOK"     COLOR_RESET "\n");break;
-                case 6 : printf(COLOR_GREEN     "(5) TOXIC ATTACK"     COLOR_RESET "\n");break;
-                case 10 : printf(COLOR_GREEN    "(6) PIRATES EYES"     COLOR_RESET "\n");break;
-                case 14 : printf(COLOR_GREEN    "(7) DEFENSIVE STANCE"     COLOR_RESET "\n");break;
-                case 1 : printf(COLOR_GREEN     "(8) SACRED SWORD"     COLOR_RESET "\n");break;
-                case 3 : printf(COLOR_GREEN     "(9) WAVE STRIKE"     COLOR_RESET "\n");break;
-                case 5 : printf(COLOR_GREEN     "(10) BACKSTAB"     COLOR_RESET "\n");break;
-                case 7 : printf(COLOR_GREEN     "(11) CAPTAINS RAGE"     COLOR_RESET "\n");break;
-                case 9 : printf(COLOR_GREEN     "(12) SUPERIOR BLOCK"     COLOR_RESET "\n");break;
-                case 11 : printf(COLOR_GREEN    "(13) WATER FORM"     COLOR_RESET "\n");break;
-                case 13 : printf(COLOR_GREEN    "(14) SALTY SKIN"     COLOR_RESET "\n");break;
-                case 15 : printf(COLOR_GREEN    "(15) ICE SHIELD"     COLOR_RESET "\n");break;
+                case 8 : printf(COLOR_GREEN     "(1) PIRATES HAT [+5 STR, +5 DEF]"     COLOR_RESET "\n");break;
+                case 4 : printf(COLOR_GREEN     "(2) CAPTAINS BLOOD [+10 STR, +5 DEF]"     COLOR_RESET "\n");break;
+                case 12 : printf(COLOR_GREEN    "(3) CAPTAINS SENSE [+5 STR, +10 DEF]"     COLOR_RESET "\n");break;
+                case 2 : printf(COLOR_GREEN     "(4) BLOODY HOOK [+20 STR, +5 DEF]"     COLOR_RESET "\n");break;
+                case 6 : printf(COLOR_GREEN     "(5) TOXIC ATTACK [+15 STR, +10 DEF]"     COLOR_RESET "\n");break;
+                case 10 : printf(COLOR_GREEN    "(6) PIRATES EYES [+10 STR, +15 DEF]"     COLOR_RESET "\n");break;
+                case 14 : printf(COLOR_GREEN    "(7) DEFENSIVE STANCE [+5 STR, +20 DEF]"     COLOR_RESET "\n");break;
+                case 1 : printf(COLOR_GREEN     "(8) SACRED SWORD [+40 STR, +5 DEF]"     COLOR_RESET "\n");break;
+                case 3 : printf(COLOR_GREEN     "(9) WAVE STRIKE [+35 STR, +10 DEF]"     COLOR_RESET "\n");break;
+                case 5 : printf(COLOR_GREEN     "(10) BACKSTAB [+30 STR, +15 DEF]"     COLOR_RESET "\n");break;
+                case 7 : printf(COLOR_GREEN     "(11) CAPTAINS RAGE [+25 STR, +20 DEF]"     COLOR_RESET "\n");break;
+                case 9 : printf(COLOR_GREEN     "(12) SUPERIOR BLOCK [+20 STR, +25 DEF]"     COLOR_RESET "\n");break;
+                case 11 : printf(COLOR_GREEN    "(13) WATER FORM [+15 STR, +30 DEF]"     COLOR_RESET "\n");break;
+                case 13 : printf(COLOR_GREEN    "(14) SALTY SKIN [+10 STR, +35 DEF]"     COLOR_RESET "\n");break;
+                case 15 : printf(COLOR_GREEN    "(15) ICE SHIELD [+5 STR, +40 DEF]"     COLOR_RESET "\n");break;
             }
         }
         else
         {
             switch (Akar(P)){
-                case 8 : printf("(1) PIRATES HAT\n");;break;
-                case 4 : printf("(2) CAPTAINS BLOOD\n");break;
-                case 12 : printf("(3) CAPTAINS SENSE\n");break;
-                case 2 : printf("(4) BLOODY HOOK\n");break;
-                case 6 : printf("(5) TOXIC ATTACK\n");break;
-                case 10 : printf("(6) PIRATES EYES\n");break;
-                case 14 : printf("(7) DEFENSIVE STANCE\n");break;
-                case 1 : printf("(8) SACRED SWORD\n");break;
-                case 3 : printf("(9) WAVE STRIKE\n");break;
-                case 5 : printf("(10) BACKSTAB\n");break;
-                case 7 : printf("(11) CAPTAINS RAGE\n");break;
-                case 9 : printf("(12) SUPERIOR BLOCK\n");break;
-                case 11 : printf("(13) SPELL ARMOR\n");break;
-                case 13 : printf("(14) HARD SKIN\n");break;
-                case 15 : printf("(15) ICE SHIELD\n");break;
+                case 8 : printf("(1) PIRATES HAT [+5 STR, +5 DEF]\n");;break;
+                case 4 : printf("(2) CAPTAINS BLOOD [+10 STR, +5 DEF]\n");break;
+                case 12 : printf("(3) CAPTAINS SENSE [+5 STR, +10 DEF]\n");break;
+                case 2 : printf("(4) BLOODY HOOK [+20 STR, +5 DEF]\n");break;
+                case 6 : printf("(5) TOXIC ATTACK [+15 STR, +10 DEF]\n");break;
+                case 10 : printf("(6) PIRATES EYES [+10 STR, +15 DEF]\n");break;
+                case 14 : printf("(7) DEFENSIVE STANCE [+5 STR, +20 DEF]\n");break;
+                case 1 : printf("(8) SACRED SWORD [+40 STR, +5 DEF]\n");break;
+                case 3 : printf("(9) WAVE STRIKE [+35 STR, +10 DEF]\n");break;
+                case 5 : printf("(10) BACKSTAB [+30 STR, +15 DEF]\n");break;
+                case 7 : printf("(11) CAPTAINS RAGE [+25 STR, +20 DEF]\n");break;
+                case 9 : printf("(12) SUPERIOR BLOCK [+20 STR, +25 DEF]\n");break;
+                case 11 : printf("(13) WATER FORM [+15 STR, +30 DEF]\n");break;
+                case 13 : printf("(14) SALTY SKIN [+10 STR, +35 DEF]\n");break;
+                case 15 : printf("(15) ICE SHIELD [+5 STR, +40 DEF]\n");break;
             }
         }
         PrintTree1(Left(P),h,inc+1);
@@ -375,59 +375,62 @@ void AmbilSkill(BinTree *P, int *STR, int *DEF)
  */
 {
     char pilih;
+    boolean stop = false;
     int x;
     int skill;
     BinTree T;
     PrintSkillTree(*P);
     printf("                                      PLAYER SKILL TREE\n");
     PrintBorder();
-    printf("If you choose the left path your STR will increase!\nIf you choose the right path your DEF will increase!\n");
+    printf("If you choose the upper path your STR will increase!\nIf you choose the lower path your DEF will increase!\n");
     printf("Pilih nomor skill yang ingin diambil :\n");
     scanf("%d",&skill);
-    while ((skill >= 16) || (skill <= 0))
+    while (!stop)
     {
-        printf("Input tidak terdefinisi, ulangi input :\n");
-        scanf("%d",&skill);
-    }
-    if (skill <= 15 && skill >= 2)
-    {
-        if (!IsSudah(*P,SkillToInt(x)))
-        {
-            T = Parent(*P,SkillToInt(skill));
-            if (IsSudah(*P,Akar(T)))
-            {
-                Sudah(SearchBST(*P,SkillToInt(skill))) = true;
-                switch (skill){
-                    case 2 : *STR += 10; *DEF += 5; printf("STR bertambah 10, DEF bertambah 5\n"); break;
-                    case 3 : *STR += 5; *DEF += 10; printf("STR bertambah 5, DEF bertambah 10\n"); break;
-                    case 4 : *STR += 20; *DEF += 5; printf("STR bertambah 20, DEF bertambah 5\n"); break;
-                    case 5 : *STR += 15; *DEF += 10; printf("STR bertambah 15, DEF bertambah 10\n"); break;
-                    case 6 : *STR += 10; *DEF += 15; printf("STR bertambah 10, DEF bertambah 15\n"); break;
-                    case 7 : *STR += 5; *DEF += 20; printf("STR bertambah 5, DEF bertambah 20\n"); break;
-                    case 8 : *STR += 40; *DEF += 5; printf("STR bertambah 40, DEF bertambah 5\n"); break;
-                    case 9 : *STR += 35; *DEF += 10; printf("STR bertambah 35, DEF bertambah 10\n"); break;
-                    case 10 : *STR += 30; *DEF += 15; printf("STR bertambah 30, DEF bertambah 15\n"); break;
-                    case 11 : *STR += 25; *DEF += 20; printf("STR bertambah 25, DEF bertambah 20\n"); break;
-                    case 12 : *STR += 20; *DEF += 25; printf("STR bertambah 20, DEF bertambah 25\n"); break;
-                    case 13 : *STR += 15; *DEF += 30; printf("STR bertambah 15, DEF bertambah 30\n"); break;
-                    case 14 : *STR += 10; *DEF += 35; printf("STR bertambah 10, DEF bertambah 35\n"); break;
-                    case 15 : *STR += 5; *DEF += 40; printf("STR bertambah 5, DEF bertambah 40\n"); break;
-                }
-            }
-            else
-            {
-                printf("Skill sebelumnya harus diambil terlebih dahulu, pengambilan skill gagal\n");
-            }
-        }
-        else
-        {
-            printf("Skill tersebut sudah diambil, pengambilan skill gagal\n");
-        }
-    }
-    else
-    {
-        printf("Skill tersebut sudah diambil, pengambilan skill gagal\n");
-    }
+		if (skill >=16 || skill <= 0)
+		{
+			printf("Input tidak terdefinisi, ulangi input :\n");
+			scanf("%d",&skill);
+		}
+		else
+		{
+				if (!IsSudah(*P,SkillToInt(x)))
+				{
+					T = Parent(*P,SkillToInt(skill));
+					if (IsSudah(*P,Akar(T)))
+					{
+						Sudah(SearchBST(*P,SkillToInt(skill))) = true;
+						switch (skill){
+							case 2 : *STR += 10; *DEF += 5; printf("STR bertambah 10, DEF bertambah 5\n"); break;
+							case 3 : *STR += 5; *DEF += 10; printf("STR bertambah 5, DEF bertambah 10\n"); break;
+							case 4 : *STR += 20; *DEF += 5; printf("STR bertambah 20, DEF bertambah 5\n"); break;
+							case 5 : *STR += 15; *DEF += 10; printf("STR bertambah 15, DEF bertambah 10\n"); break;
+							case 6 : *STR += 10; *DEF += 15; printf("STR bertambah 10, DEF bertambah 15\n"); break;
+							case 7 : *STR += 5; *DEF += 20; printf("STR bertambah 5, DEF bertambah 20\n"); break;
+							case 8 : *STR += 40; *DEF += 5; printf("STR bertambah 40, DEF bertambah 5\n"); break;
+							case 9 : *STR += 35; *DEF += 10; printf("STR bertambah 35, DEF bertambah 10\n"); break;
+							case 10 : *STR += 30; *DEF += 15; printf("STR bertambah 30, DEF bertambah 15\n"); break;
+							case 11 : *STR += 25; *DEF += 20; printf("STR bertambah 25, DEF bertambah 20\n"); break;
+							case 12 : *STR += 20; *DEF += 25; printf("STR bertambah 20, DEF bertambah 25\n"); break;
+							case 13 : *STR += 15; *DEF += 30; printf("STR bertambah 15, DEF bertambah 30\n"); break;
+							case 14 : *STR += 10; *DEF += 35; printf("STR bertambah 10, DEF bertambah 35\n"); break;
+							case 15 : *STR += 5; *DEF += 40; printf("STR bertambah 5, DEF bertambah 40\n"); break;
+						}
+					stop = true;
+					}
+					else
+					{
+						printf("Skill sebelumnya harus diambil terlebih dahulu, pengambilan skill gagal\n");
+						scanf("%d",&skill);
+					}
+				}
+				else
+				{
+					printf("Skill tersebut sudah diambil, pengambilan skill gagal\n");
+					scanf("%d",&skill);
+				}
+		}
+	}
 }
 
 /* Command ini digunakan untuk menampilkan skill yang sudah diambil, dan
