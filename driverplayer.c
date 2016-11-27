f3fbc3468e6508a1a4a8d6f7c41f8583d8fe9f2c
audrynyonata <audrynyonata@gmail.com>
2016-11-28 02:35:11 +0700

    Add files via upload

--- driverplayer.c
+++ driverplayer.c
@@ -1,4 +1,4 @@
-/* namafile: driver.c */
+/* namafile: driverplayer.c */
 /* cara compile : gcc -o driver.exe driver.c matriks.c player.c point.c mesinkata.c mesinkar.c*/
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,40 +7,43 @@
 #include "boolean.h"
 #include "mesinkata.h"
 #include "point.h"
-#define cplayer 'P'
-
-void clrscr()
-{
-	system("@cls||clear");
-}
 
 int main(){
-	MATRIKS M1,M2,M3,M4,M5;
-	Kata X; 
-	PLAYER P1; 
-	char menu;
+	/* KAMUS */
+	PLAYER P1;
+	Kata name;
+	char C;
+	int i = 0;
 	
-	X.TabKata[0]='A';
-	X.Length=1;
-	CreateEmptyPlayer(&P1,X);
-    BacaMATRIKS(&M1,&M2,&M3,&M4,&M5,20,20);
-
+	/* ALGORITMA */
 	clrscr();
-	InitiatePos(&P1,&M1,0);
-	TulisMATRIKS(M1);
-	printf("\n");
+	printf("Masukkan nama player: ");
+	name.Length=0;
 	do
 	{
-		menu = 'F';
-		printf("Input Command: ");
-		BacaCommand(&P1,&M1,&menu);
-		if (menu =='G')
+		scanf("%c", &C);
+		if (C!='\n')
 		{
-			clrscr();
-			TulisMATRIKS(M1); 
-			printf("\n");
-		}	
+			name.TabKata[i]=C;
+			name.Length++;
+			i++;
+		}
 	}
-	while (menu!='E');
-return 0;
+	while (C!='\n');
+	CreateEmptyPlayer(&P1, name);
+	//SwitchPos
+	//Jalan
+	PrintGame(P1);
+	PHP(P1)=75;
+	printf("HP: %d, MaxHP : %d, STR : %d, DEF : %d, EXP : %d, Level : %d\n", PHP(P1), MaxHP(P1), PSTR(P1), PDEF(P1), PEXP(P1), PLevel(P1));
+	printf("Darah Naik: \n");
+	DarahNaik(&P1);
+	printf("HP: %d, MaxHP : %d, STR : %d, DEF : %d, EXP : %d, Level : %d\n", PHP(P1), MaxHP(P1), PSTR(P1), PDEF(P1), PEXP(P1), PLevel(P1));
+	printf("PrintCLoop('*',5) : ");
+	PrintCLoop('*',5);
+	printf("\n");
+	printf("Digit(1234) : %d\n", digit(1234) );
+	
+	return 0;
 }
+	
