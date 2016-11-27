060b154a7c50297b7605eb5f8791753e03b64a77
aliviaprht <liviagustus@live.com>
2016-11-28 04:21:19 +0700

    :)

---
+++ mgambar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "gambar.h"
+
+int main () {
+	
+	PrintCredits();
+	printf("\n");
+	PrintCoolBS();
+	printf("\n");
+	PrintLose();
+	printf("\n");
+	PrintWin();
+	
+	return 0;
+}
